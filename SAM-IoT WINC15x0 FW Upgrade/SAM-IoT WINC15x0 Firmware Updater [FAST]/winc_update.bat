7b48c6013ec1fb265b1d3a6905b4a7109a86c25d
Randy Wu <randywu763@gmail.com>
2021-01-28 12:03:29 -0800

    Add files via upload

---
+++ SAM-IoT WINC15x0 FW Upgrade/SAM-IoT WINC15x0 Firmware Updater [FAST]/winc_update.bat
@@ -0,0 +1,2 @@
+REM Set the last (right-most) number to the Virtual COM port of the board (e.g. '3' = COM3)
+download_all UART SAMD21 3A0 0 3
---
+++ SAM-IoT WINC15x0 FW Upgrade/SAM-IoT WINC15x0 Firmware Updater [SLOW]/winc_update.bat
@@ -0,0 +1,2 @@
+REM Set the number below to the Virtual COM port of the board (e.g. '3' = COM3)
+download_all_uart 3
