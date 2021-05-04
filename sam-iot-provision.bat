ae45523e720fe121f11e4b90b083c3f39a8afe9f
Randy Wu - C14166 <randywu763@gmail.com>
2021-05-03 21:44:45 -0700

    Output provisioning duration calculation

--- sam-iot-provision.bat
+++ sam-iot-provision.bat
@@ -13,11 +13,13 @@ SET DIR_CREDENTIALS=ChainOfTrust
 
 REM <Display the current date and time>
 ECHO.
-ECHO Provisioning Script STARTED on
-date /T
-echo at
-time /T
 ECHO.
+ECHO Provisioning Script STARTED at %TIME% (%DATE%)
+set startHH24=%TIME: =0%
+set startHH24=%HH:~0,2%
+set startMI=%TIME:~3,2%
+set startSS=%TIME:~6,2%
+set startFF=%TIME:~9,2%
 
 ECHO.
 ECHO *** Programming Serial Bridge Firmware into MCU ***
@@ -27,11 +29,10 @@ copy .\%DIR_UPGRADER%\sam_iot_serial_bridge.hex %drive%:\
 ECHO.
 ECHO *** Launching WINC Programming Utilities ***
 ECHO.
-ECHO *** (Now might be a good time for a 15-minute coffee break) ***
+ECHO *** (Now might be a good time for a coffee break) ***
 ECHO.
 cd .\%DIR_UPGRADER%\%wincver%
-IF %wincver%==19.6.5 (CALL download_all.bat UART SAMD21 3A0 0 %comport%)
-IF %wincver%==19.7.3 (CALL download_all.bat UART 1500 0 %comport%)
+IF %wincver% LSS 19.7.3 (CALL download_all.bat UART SAMD21 3A0 0 %comport%) ELSE (CALL download_all.bat UART 1500 0 %comport%)
 
 ECHO.
 ECHO *** Programming Provisioning Firmware into MCU ***
@@ -111,8 +112,15 @@ ECHO *** Provisioning SAM-IoT Development Board COMPLETED ***
 ECHO.
 
 REM <Display the current date and time>
-ECHO Provisioning Script ENDED on
-date /T
-echo at
-time /T
+ECHO Provisioning Script ENDED at %TIME% (%DATE%)
+set endHH24=%TIME: =0%
+set endHH24=%HH:~0,2%
+set endMI=%TIME:~3,2%
+set endSS=%TIME:~6,2%
+set endFF=%TIME:~9,2%
+
+REM <Calculate duration of provisioning process in minutes>
+ECHO.
+SET /A minutes = (endMI - startMI)
+IF %minutes%==0 (ECHO The total provisioning time took less than a minute which means something went wrong...) ELSE (ECHO The total provisioning time took approximately %minutes% mins)
 ECHO.
